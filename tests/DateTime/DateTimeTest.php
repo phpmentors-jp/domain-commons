0d4b6c4669a70af921b4826262d94bfeb1136f0e
hidenorigoto <hidenorigoto@gmail.com>
2017-06-01 00:05:31 +0900

    [DateTime] add/split addMonths tests

--- tests/DateTime/DateTimeTest.php
+++ tests/DateTime/DateTimeTest.php
@@ -16,6 +16,9 @@ class DateTimeTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @test
+     * @param string $dateStr
+     * @param int    $days
+     * @param string $expected
      * @dataProvider addDaysData
      */
     public function addDays($dateStr, $days, $expected)
@@ -37,15 +40,14 @@ class DateTimeTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @test
-     * @dataProvider addMOnthsData
+     * @param string  $dateStr
+     * @param int     $months
+     * @param string  $expected
+     * @dataProvider  addMonthsData
      */
     public function addMonths($dateStr, $months, $expected)
     {
         $date = new DateTime($dateStr);
-
-        if ($expected === null) {
-            $this->setExpectedException('RuntimeException');
-        }
 
         $added = $date->addMonths($months);
 
@@ -56,9 +58,36 @@ class DateTimeTest extends \PHPUnit_Framework_TestCase
     {
         return [
             ['2015-03-21',  3, '2015-06-21'],
-            ['2015-01-30',  1, null],
+            ['2017-05-30',  1, '2017-06-30'],
             ['2014-12-19',  5, '2015-05-19'],
             ['2014-12-19', -2, '2014-10-19'],
+        ];
+    }
+
+    /**
+     * @test
+     * @param string  $dateStr
+     * @param int     $months
+     * @dataProvider  addMonthsThrowsExceptionData
+     * @expectedException \PHPMentors\DomainCommons\DateTime\Exception\UnsupportedCalculation
+     */
+    public function addMonthsThrowsException($dateStr, $months)
+    {
+        $date = new DateTime($dateStr);
+
+        $date->addMonths($months);
+
+        $this->fail();
+    }
+
+    public function addMonthsThrowsExceptionData()
+    {
+        return [
+            ['2015-01-30',  1],
+            ['2017-05-31',  1],
+            ['2017-01-29',  1],
+            ['2017-01-30',  1],
+            ['2017-01-31',  1],
         ];
     }
 }
